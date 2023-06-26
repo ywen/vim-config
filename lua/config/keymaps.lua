c90ca5f5db6e23fcd2bcbc6eaec538f481dbd6b9
Yi Wen <ywen.github@gmail.com>
2023-06-25 19:01:53 -0700

    More config

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -3,3 +3,10 @@
 -- Add any additional keymaps here
 
 vim.api.nvim_set_keymap("n", ";", ":", { noremap = true })
+vim.api.nvim_set_keymap("n", "fb", ":b#<CR>", { noremap = true })
+
+-- split window and navigate keys
+vim.keymap.set("n", "<leader>v", "<C-w>v<C-w>l")
+vim.keymap.set("n", "<leader>w", "<C-w>s<C-w>l")
+vim.keymap.set("n", "<C-b>", "<ESC>:BufOnly<cr>")
+vim.keymap.set("n", "<C-l>", "<ESC>:buffer ")
